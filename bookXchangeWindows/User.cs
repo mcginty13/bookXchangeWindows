cbfe9e1cf3d05638ee0fdc8ce84a91ca00fddea9
511760 <511760@FEN-052-092>
2017-02-24 15:36:17 +0000

    User Class

---
+++ bookXchangeWindows/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookXchangeWindows
+{
+    class User
+    {
+        private string mID;
+        private string mName;
+        private string mEmail;
+        private string mCourse;
+        private int mYear;
+        private List<Book> bookRequested;
+        private List<Book> bookForSale;
+
+
+        public User(string pID ,string pName, string pEmail, string pCourse, int pYear)
+        {
+            string mID = pID;
+            string mName = pName;
+            string mEmail = pEmail;
+            string mCourse = pCourse;
+            int mYear = pYear;
+            bookRequested = new List<Book>();
+            bookForSale = new List<Book>();
+        }
+    }   
+}
